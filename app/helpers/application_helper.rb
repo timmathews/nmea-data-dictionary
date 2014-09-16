b644aee92258381dcdc327efc0f8a161e998bb11
Tim Mathews <tmathews04@gmail.com>
2014-09-16 18:45:59 -0400

    Added markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown(text)
+    return "" if text.nil?
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+
+    markdown.render(text).html_safe
+  end
 end
