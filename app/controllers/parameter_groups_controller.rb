eed5caff392280308b125423e9feb1af82622ee0
Tim Mathews <tmathews04@gmail.com>
2014-09-29 16:39:20 -0400

    Rewrite parameters when editing pgns to autolayout fields

--- app/controllers/parameter_groups_controller.rb
+++ app/controllers/parameter_groups_controller.rb
@@ -24,6 +24,8 @@ class ParameterGroupsController < ApplicationController
   # POST /parameter_groups
   # POST /parameter_groups.json
   def create
+    set_field_metadata parameter_group_params
+
     @parameter_group = ParameterGroup.new(parameter_group_params)
 
     respond_to do |format|
@@ -40,6 +42,8 @@ class ParameterGroupsController < ApplicationController
   # PATCH/PUT /parameter_groups/1
   # PATCH/PUT /parameter_groups/1.json
   def update
+    params = set_field_metadata parameter_group_params
+
     respond_to do |format|
       if @parameter_group.update(parameter_group_params)
         format.html { redirect_to @parameter_group, notice: 'Parameter group was successfully updated.' }
@@ -69,6 +73,34 @@ class ParameterGroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def parameter_group_params
-      params.require(:parameter_group).permit(:name, :description, :pgn, :priority, :default_rate, :database_version, :pgn_category_id, :pgn_type_id, :size, :repeating_fields, :is_known, parameter_group_fields_attributes: [:id, :field_id, :parameter_group_id, :order, :byte_position, :bit_position])
+      params.require(:parameter_group).permit(:name, :description, :pgn, :priority, :default_rate, :database_version, :pgn_category_id, :pgn_type_id, :size, :repeating_fields, :is_known, parameter_group_fields_attributes: [:id, :field_id, :parameter_group_id, :order, :byte_position, :bit_position, :_destroy])
+    end
+
+    # On create or udpate, we need to specify order and bit/byte position fields
+    def set_field_metadata(params)
+      by = 1
+      bi = 1
+      tb = 0
+      o = 1
+
+      return params if params["parameter_group_fields_attributes"].nil?
+
+      params["parameter_group_fields_attributes"].each do |k, v|
+
+        next if v["_destroy"] == "true"
+
+        v["order"] =  o.to_s
+        v["byte_position"] = by.to_s
+        v["bit_position"] = bi.to_s
+
+        fl = Field.find(v["field_id"].to_i)
+
+        tb += fl.length
+        by = (tb.to_f / 8).floor + 1
+        bi = tb % 8 + 1
+        o += 1
+      end
+
+      return params
     end
 end
